f49714288e326dc2014aacf52ed0fe44b5d50fee
shaniatiass <shani.attias.080501@gmail.com>
2026-01-08 15:50:36 +0200

    Add Button sava and cancel

--- app/src/main/java/com/example/studentapp/AddStudentActivity.kt
+++ app/src/main/java/com/example/studentapp/AddStudentActivity.kt
@@ -1,6 +1,9 @@
 package com.example.studentapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,26 @@ class AddStudentActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val addStudentButton: Button = findViewById(R.id.add_student_save_student_button)
+        val cancelButton: Button = findViewById(R.id.add_student_cancel_button)
+
+        val studentNameEditText: EditText = findViewById(R.id.add_student_name_edit_text)
+        val studentIdEditText: EditText = findViewById(R.id.add_student_id_edit_text)
+
+        val saveStudentTextView: TextView = findViewById(R.id.add_student_save_status_text_view)
+
+        cancelButton.setOnClickListener {
+            finish()
+        }
+
+        addStudentButton.setOnClickListener {
+            val studentName = studentNameEditText.text.toString()
+            val studentId = studentIdEditText.text.toString()
+
+            saveStudentTextView.text = "Student Save: Name = $studentName, ID = $studentId"
+            studentNameEditText.text.clear()
+            studentIdEditText.text.clear()
+        }
+
     }
 }
