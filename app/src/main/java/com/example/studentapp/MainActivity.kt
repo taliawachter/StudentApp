4ae6dcf844846a39dcd68eda64f8cc83d759992a
taliawachter <165553495+taliawachter@users.noreply.github.com>
2026-01-08 14:45:34 +0200

    Add AddStudent screen

--- app/src/main/java/com/example/studentapp/MainActivity.kt
+++ app/src/main/java/com/example/studentapp/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.studentapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val button = findViewById<Button>(R.id.main_add_student_button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, AddStudentActivity::class.java)
+            startActivity(intent)
         }
     }
 }
